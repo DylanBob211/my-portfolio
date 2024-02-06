67c5a28a25cbe545728f10b8331069b3dfcb9040
Nicola D'Oronzo <nicoladoronzo95@gmail.com>
2024-02-06 07:38:44 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate, facilis quo veritatis iure saepe, impedit nemo aut magni adipisci reiciendis maiores optio, itaque dolores voluptatem quidem odit! Expedita, unde.
+    </main>
+  );
+}
