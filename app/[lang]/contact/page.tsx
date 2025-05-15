a8ef4da13652a29174059c92f6570107be884dab
Nicola D'Oronzo <n.doronzo95@gmail.com>
2025-05-15 12:24:35 +0200

    resend implemented

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -1,9 +1,13 @@
 import { Metadata } from 'next'
 import { getDictionary } from '../dictionaries'
+import { Resend } from 'resend'
+import EmailTemplate from '@/app/ui/email'
 
 export const metadata: Metadata = {
   title: 'Contact',
 }
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function Contact({
   params,
@@ -12,10 +16,23 @@ export default async function Contact({
 }) {
   const resolvedParams = await params
   const dict = await getDictionary(resolvedParams.lang)
-
+  async function sendEmail(formData: FormData) {
+    'use server'
+    const emailData = {
+      name: formData.get('name') as string,
+      email: formData.get('email') as string,
+      message: formData.get('message') as string,
+    }
+    await resend.emails.send({
+      from: 'My contact page',
+      to: 'nicoladoronzo95@gmail.com',
+      subject: `${emailData.name} ti cerca`,
+      react: await EmailTemplate(emailData),
+    })
+  }
   return (
     <main className="sm:w-2/5 lg:w-1/5">
-      <form className="flex flex-col gap-4">
+      <form action={sendEmail} className="flex flex-col gap-4">
         <label className="flex flex-col">
           <span>{dict.contact.nameLabel}</span>
           <input
---
+++ app/ui/email.tsx
@@ -0,0 +1,22 @@
+import { em } from 'motion/react-client'
+import * as React from 'react'
+
+interface EmailTemplateProps {
+  name: string
+  email: string
+  message: string
+}
+
+const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  name,
+  email,
+  message,
+}) => (
+  <div>
+    <h1>
+      {name} - {email} ti ha scritto!
+    </h1>
+    <p>{message}</p>
+  </div>
+)
+export default EmailTemplate
