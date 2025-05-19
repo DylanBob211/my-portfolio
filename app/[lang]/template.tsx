81fdbca18468ac37a853eb085dca0098af24f42d
Nicola D'Oronzo <n.doronzo95@gmail.com>
2025-05-19 17:41:47 +0200

    reduce delay

--- app/[lang]/template.tsx
+++ app/[lang]/template.tsx
@@ -10,7 +10,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       initial={{ opacity: 0, y: 5 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 5 }}
-      transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.8 }}
+      transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.4 }}
       className="h-full w-full"
     >
       {children}
